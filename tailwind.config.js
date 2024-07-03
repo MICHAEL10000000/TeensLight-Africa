73d16090fa494ccb172db99e4125a6778140e1c6
Gbenga <divadaforbusiness@gmail.com>
2024-07-03 04:18:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+       "RobotoB" : ["RobotoB"],
+        "RobotoM" : ["RobotoM"],
+        "RobotoR" : ["RobotoR"], 
+        "RobotoI" : ["RobotoI"]
+      }
+    },
+  },
+  plugins: [],
+}
+
